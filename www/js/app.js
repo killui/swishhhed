5b3a92d4c81e1a36c65e00e05c2d33f444ec235d
killui <quentin.lemoine@etudiant-eemi.com>
2015-02-18 21:18:57 +0100

    Add page shots, for one shot, and info user

--- www/js/app.js
+++ www/js/app.js
@@ -3,9 +3,10 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic', 'starter.controllers'])
+angular.module('swishhhed', ['ionic', 'starter.controllers'])
 
 .run(function($ionicPlatform) {
+
   $ionicPlatform.ready(function() {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs)
@@ -16,4 +17,27 @@ angular.module('starter', ['ionic', 'starter.controllers'])
       StatusBar.styleDefault();
     }
   });
+
+})
+
+.config(function($stateProvider, $urlRouterProvider) {
+
+  $stateProvider
+  .state('index', {
+      url: '/',
+      templateUrl: 'templates/shots.html',
+      controller: 'shotsController'
+  })
+  .state('shots', {
+    url: '/shot/:shotId',
+    templateUrl: 'templates/shot.html',
+    controller: 'shotController'
+  })
+  .state('profil', {
+    url: '/profil',
+    templateUrl: 'templates/profil.html',
+    controller: 'profilController'
+  })
+  ;
+  $urlRouterProvider.otherwise('/');
 })
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,8 +1,8 @@
 angular.module('starter.controllers', [])
 
-.controller('profilInformationController', function ($scope,$http) {
+.controller('shotsController', function ($scope,$http) {
     var OAUTH_TOKEN = "f59e506fd880352413323c6b53cb72c91e2b2ec2c6fa7da64a5250e2484c891c";
-    var urlUser = "https://api.dribbble.com/v1/users/lemoine_quentin";
+    var url = "https://api.dribbble.com/v1/shots?sort=views";
     var config = {
         headers: {
             'Content-Type': 'application/json, text/html, application/javascript',
@@ -12,11 +12,92 @@ angular.module('starter.controllers', [])
         withCredentials: true
     };
 
-    $http.get(urlUser,config).
+    $http.get(url,config).
     success(function(data) {
         // this callback will be called asynchronously
         // when the response is available
-        $scope.test = "success";
+        $scope.data = data;
+    }).
+    error(function(err) {
+        // called asynchronously if an error occurs
+        // or server returns response with an error status.
+        console.log(err);
+    });
+
+})
+
+.controller('shotController', function ($scope,$http,$stateParams) {
+    var OAUTH_TOKEN = "f59e506fd880352413323c6b53cb72c91e2b2ec2c6fa7da64a5250e2484c891c";
+    var shotId = $stateParams.shotId;
+    console.log(shotId);
+    var url = "https://api.dribbble.com/v1/shots/"+shotId;
+    var config = {
+        headers: {
+            'Content-Type': 'application/json, text/html, application/javascript',
+            'Authorization': 'Bearer '+OAUTH_TOKEN,
+            'Origin': '*'
+        },
+        withCredentials: true
+    };
+
+    $http.get(url,config)
+    .success(function(data) {
+        // this callback will be called asynchronously
+        // when the response is available
+        $scope.title = data.title;
+
+        if (data.images.hidpi == null) {
+            $scope.shotImages = data.images.normal;
+        } else {
+            $scope.shotImages = data.images.hidpi;
+        }
+
+        $scope.views = data.views_count;
+        $scope.likes = data.likes_count;
+        $scope.comments = data.comments_count;
+
+        var urlComments = data.comments_url;
+
+        $http.get(urlComments,config)
+        .success(function(comments) {
+            $scope.comment = comments;
+        })
+        .error(function(err) {
+            console.log(err);
+        });
+        
+    })
+    .error(function(err) {
+        // called asynchronously if an error occurs
+        // or server returns response with an error status.
+        console.log(err);
+    });
+
+})
+
+.controller('profilController', function ($scope,$http) {
+    var OAUTH_TOKEN = "f59e506fd880352413323c6b53cb72c91e2b2ec2c6fa7da64a5250e2484c891c";
+    var url = "https://api.dribbble.com/v1/user?access_token="+OAUTH_TOKEN;
+    var config = {
+        headers: {
+            'Content-Type': 'application/json, text/html, application/javascript',
+            'Authorization': 'Bearer '+OAUTH_TOKEN,
+            'Origin': '*'
+        },
+        withCredentials: true
+    };
+
+    $http.get(url,config).
+    success(function(data) {
+        // this callback will be called asynchronously
+        // when the response is available
+        $scope.id = data.id;
+        $scope.name = data.name;
+        $scope.avatar = data.avatar_url;
+        $scope.followers = data.followers_count;
+        $scope.followings = data.followings_count;
+        $scope.likes = data.likes_count;
+
         console.log(data);
     }).
     error(function(err) {
@@ -26,6 +107,5 @@ angular.module('starter.controllers', [])
         console.log(err);
     });
 
-    
-    document.getElementByIt('data').text('data');
-});
+})
+;
